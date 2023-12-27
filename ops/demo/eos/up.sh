adf906bac74817be698cdf6fb707a32c23f406a4
lammaxcool <lammaxcool@gmail.com>
2023-12-27 22:48:04 +0200

    Reduce amount of mock data

--- ops/demo/eos/up.sh
+++ ops/demo/eos/up.sh
@@ -7,10 +7,10 @@ cd $(dirname $0)/../..
 ACTIVE_BROKERS=$(sed -n -E -e "s/^  (broker-[0-9]):/\1 /p" ./kafka/docker-compose.yml | tr -d '\n')
 (cd kafka; docker compose up -d zookeeper ${ACTIVE_BROKERS})
 
-(cd connect; docker compose up -d)
+(cd connect; docker compose up --build -d)
 
-(cd druid; docker compose up -d)
+(cd druid; docker compose up --build -d)
 
-(cd ksqlDB; docker compose up -d)
+(cd ksqlDB; docker compose up --build -d)
 
-(cd monitoring; docker compose up -d prometheus grafana)
+(cd monitoring; docker compose up --build -d prometheus grafana)
