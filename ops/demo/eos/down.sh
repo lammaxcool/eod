533f442899315a15ec4d1bcc971d39f7fe96b03e
lammaxcool <lammaxcool@gmail.com>
2023-12-27 21:54:01 +0200

    Remove broker-4 and connect-2 services

--- ops/demo/eos/down.sh
+++ ops/demo/eos/down.sh
@@ -2,12 +2,8 @@
 
 cd $(dirname $0)/../..
 
-# in case they were started
-(cd dashboards; docker compose down -v)
 (cd monitoring; docker compose down -v)
-
 (cd connect; docker compose down -v)
 (cd druid; docker compose down -v)
 (cd ksqlDB; docker compose down -v)
-(cd kafka-lb; docker compose down -v)
 (cd kafka; docker compose down -v)
