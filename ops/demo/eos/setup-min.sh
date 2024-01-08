e6c04c6d4dd8960c80da5982572fa808f7b715f5
lammaxcool <lammaxcool@gmail.com>
2024-01-08 04:00:36 +0200

    Roll up minimal config to reduce load on host

---
+++ ops/demo/eos/setup-min.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+DIR=$(dirname $0)
+
+cd $DIR
+echo "dir {$DIR}"
+
+BIN=$DIR/../../bin
+
+CONNECT=$BIN/connect
+RESTART_KSQL=$BIN/timed-ksql-restart
+STOP_KSQL=$BIN/timed-ksql-stop
+
+KT="docker exec -t cp-kafka-tools kafka-topics --bootstrap-server broker-1:9092,broker-2:9092,broker-3:9092"
+KSQL="docker exec -t cp-kafka-tools ksql http://ksql-server:8088 --config-file /config/ksql.properties --file "
+
+$CONNECT available
+[ $? -ne 0 ] && echo "connector RESTful API is not yet available, aborting script. wait until connector is ready to run this script." && exit 1
+echo "connector RESTful API AVAILABLE "
+
+# also done in build.sh
+cp -r data/* ../../connect/data
+echo "copying data executed"
+
+#docker exec -t cp-kafka-tools kafka-topics --bootstrap-server broker-1:9092,broker-2:9092,broker-3:9092 --create --replication-factor 3 --partitions 4 --topic orders
+${KT} --create --replication-factor 3 --partitions 4 --topic orders
+echo "create topic orders executed"
+${KT} --create --replication-factor 3 --partitions 4 --topic ORDERS_ENRICHED
+echo "create topic ORDERS_ENRICHED"
+${KT} --create --replication-factor 3 --partitions 4 --topic users
+echo "create topic users executed"
+${KT} --create --replication-factor 3 --partitions 4 --topic stores
+echo "create topic stores executed"
+
+$CONNECT plugins
+[ $? -eq 1 ] && error_msg "connect RESTful API not yet available" && exit 1
+echo "connect plugins executed"
+
+$CONNECT create ./connectors/store.json
+echo "connect create connector store.json executed"
+$CONNECT create ./connectors/user.json
+echo "connect create connector user.json executed"
+$CONNECT create ./connectors/order.json
+echo "connect create connector order.json executed"
+
+#docker exec -t cp-kafka-tools ksql http://ksql-server:8088 --config-file /config/ksql.properties --file /ksql/users.ksql
+$KSQL /ksql/users.ksql
+echo "ksql users.ksql executed"
+$KSQL /ksql/stores.ksql
+echo "ksql stores.ksql executed"
+$KSQL /ksql/orders.ksql
+echo "ksql orders.ksql executed"
+
+echo "sleeping 2 seconds to make sure the tables for users and stores are hydrated before orders enrichment stream is created"
+sleep 2
+$KSQL /ksql/orders_enriched.ksql
+echo "ksql orders_enriched.ksql executed"
+
+$RESTART_KSQL 30 10
+$STOP_KSQL 50
---
+++ ops/demo/eos/up-min.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+cd $(dirname $0)/../..
+
+./network.sh
+
+KSQLDB_DOCKER_COMPOSE_FILE="ksqlDB/docker-compose.yml"
+
+for i in "$@"; do
+  case $i in
+    -e|--eos|--exactly-once)
+      eos=true
+      ;;
+    -a|--alo|--at-least-once)
+      alo=true
+      ;;
+  esac
+done
+
+if [[ "$eos" == true ]]; then
+  sed -I .bak -E "s/KSQL_KSQL_STREAMS_PROCESSING_GUARANTEE: (.*)/KSQL_KSQL_STREAMS_PROCESSING_GUARANTEE: exactly_once_v2/" ${KSQLDB_DOCKER_COMPOSE_FILE}
+  echo ""
+  echo "docker-compose.yml updated for $CONTAINER, processing guarantee set to exactly_once_v2"
+  echo ""
+elif [[ "$alo" == true ]]; then
+  sed -I .bak -E "s/KSQL_KSQL_STREAMS_PROCESSING_GUARANTEE: (.*)/KSQL_KSQL_STREAMS_PROCESSING_GUARANTEE: at_least_once/" ${KSQLDB_DOCKER_COMPOSE_FILE}
+  echo ""
+  echo "docker-compose.yml updated for $CONTAINER, processing guarantee set to at_least_once"
+  echo ""
+fi
+rm -f "${KSQLDB_DOCKER_COMPOSE_FILE}.bak"
+
+(cd cp-kafka-tools; docker compose up -d)
+
+ACTIVE_BROKERS=$(sed -n -E -e "s/^  (broker-[0-9]):/\1 /p" ./kafka/docker-compose.yml | tr -d '\n')
+(cd kafka; docker compose up -d zookeeper ${ACTIVE_BROKERS})
+
+(cd connect; docker compose up --build -d)
+
+(cd ksqlDB; docker compose up --build -d)
